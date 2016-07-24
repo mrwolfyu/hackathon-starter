f6c5ffc42b89f8d38d803d99be36554b6e47b90f
Marko Dzida <marko@dzida.net>
2016-07-24 13:07:20 +0200

    bower static

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.use(function(req, res, next) {
   next();
 });
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
-
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 /**
  * Primary app routes.
  */
