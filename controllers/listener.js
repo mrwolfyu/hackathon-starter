e0a09d49fbf12eed3c81ed12c4d2a16889177c7e
Marko Dzida <marko.dzida@int.smatsa.yu>
2017-07-13 15:40:26 +0200

    bvcb df

--- controllers/listener.js
+++ controllers/listener.js
@@ -68,7 +68,7 @@ exports.getRecordings = (req, res) => {
     var url = utils.urlbuilder('getRecordings','');
 
     
-
+ console.log("ASDASDSDAD " + req.user.profile.roomID);
      Room.findById(req.user.profile.roomID, function (err, room){
          console.log("ASDASDSDAD " + room.meetingID);
         var pRec = new Promise((resolve, reject) =>{
