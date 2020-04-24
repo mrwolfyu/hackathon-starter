77bf4e8a3114f775ffced2d3c68e20e7a953dc5d
mrwolfyu <marko@dzida.net>
2020-04-24 13:07:57 +0200

    Update adduser.js

--- for-deploy/adduser.js
+++ for-deploy/adduser.js
@@ -1,13 +1,13 @@
-var user = {  "password" : "$2a$10$uemko49LBB.nFI6wLqLNGuSKSsVlVY8PIs6c6kwHdFty/Juzu8.ZG", "profile" : { "name" : "adminc", "roomID" : "", "tip" : "admin", "xml" : "" }, "username" : "admin" };
+var user = {  "password" : "$2a$10$uemko49LBB./Juzu8.ZG", "profile" : { "name" : "adminc", "roomID" : "", "tip" : "admin", "xml" : "" }, "username" : "admin" };
 
 
 db.users.insert(user);
 
-var user = {  "username" : "dzida", "password" : "$2a$10$cRbGZXG/FiVgfgqscE9.QustJ17mH2uTH0jcJAlZUexZ93CfR73/6", "profile" : { "roomID" : "", "xml" : "", "tip" : "admin", "name" : "Marko Dzida" }, "__v" : 0};
+var user = {  "username" : "dzida", "password" : "$2a$10$cRbGZXG/./6", "profile" : { "roomID" : "", "xml" : "", "tip" : "admin", "name" : "Marko Dzida" }, "__v" : 0};
 
 
 db.users.insert(user);
 
 
-{  "password" : "$2a$10$uemko49LBB.nFI6wLqLNGuSKSsVlVY8PIs6c6kwHdFty/Juzu8.ZG", "profile" : { "name" : "adminc", "roomID" : "", "tip" : "listener", "xml" : "" }, "username" : "test" };
+{  "password" : "asd./Juzu8.ZG", "profile" : { "name" : "adminc", "roomID" : "", "tip" : "listener", "xml" : "" }, "username" : "test" };
 
